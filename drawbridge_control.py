c79529735ea4f7c6a15f4e825f70006ae0947bbd
Nicolas Racicot <racicot.nicolas2@gmail.com>
2023-05-31 18:00:54 -0400

    Basic control logic implemented and testing features. Ready for HW tests

---
+++ drawbridge_control.py
@@ -0,0 +1,67 @@
+import time
+import threading
+
+
+class Drawbridge:
+
+    def __init__(self, ismoving, isup, isdown, isfailed):
+        self.ismoving = ismoving
+        self.isup = isup
+        self.isdown = isdown
+        self.isfailed = isfailed
+
+    def getstate(self):
+        print("In transit: " + str(self.ismoving))
+        print("Uplock: " + str(self.isup))
+        print("Downlock: " + str(self.isdown))
+        print("Failed: " + str(self.isfailed))
+
+    # Drawbridge Up Command Method
+    def commandup(self):
+
+        print("Commanded up!")
+        if self.ismoving:
+            print("ERROR: BRIDGE IN TRANSIT!")
+        elif self.isup:
+            print("ERROR: BRIDGE IS ALREADY UP!")
+
+        def moveup():
+            while self.ismoving and not self.isup:
+                time.sleep(.5)
+                print("Moving up!")
+                if self.isup:
+                    print("Reached Uplock!")
+                    self.ismoving = False
+
+        if not self.ismoving and not self.isup:
+            self.ismoving = True
+            t = threading.Thread(target=moveup)
+            t.start()
+
+    # Drawbridge Stop Method
+    def stop(self):
+        if self.ismoving:
+            self.ismoving = False
+            print("STOP!")
+
+    # Drawbridge Down Command Method
+    def commanddown(self):
+
+        print("Commanded down!")
+        if self.ismoving:
+            print("ERROR: BRIDGE IN TRANSIT!")
+        elif self.isdown:
+            print("ERROR: BRIDGE IS ALREADY DOWN!")
+
+        def movedown():
+            while self.ismoving and not self.isdown:
+                time.sleep(.5)
+                print("Moving down!")
+                if self.isdown:
+                    print("Reached Downlock!")
+                    self.ismoving = False
+
+        if not self.ismoving and not self.isdown:
+            self.ismoving = True
+            t = threading.Thread(target=movedown)
+            t.start()
--- main_app.py
+++ main_app.py
@@ -1,28 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
-import control
-from control import moving
+from drawbridge_control import Drawbridge
 
+# Defining Drawbridge object
+
+Bridge = Drawbridge(False, False, False, False)
 
 # Creating Main App
 
 app = tk.Tk()
 app.title("DBController")
-app.geometry("250x100")
+app.geometry("250x200")
+
+
+# Defining function for window close
+def on_closing():
+    Bridge.stop()
+    app.destroy()
+
 
 # Buttons
 
 up_button = ttk.Button(
     app,
     text="UP",
-    command=control.command_up
+    command=Bridge.commandup
 )
 up_button.pack()
 
 stop_button = ttk.Button(
     app,
     text="STOP",
-    command=control.stop
+    command=Bridge.stop
 
 )
 stop_button.pack()
@@ -30,10 +39,86 @@ stop_button.pack()
 down_button = ttk.Button(
     app,
     text="DOWN",
+    command=Bridge.commanddown
 
 )
 down_button.pack()
 
-# App Main Loop
-app.mainloop()
+getstate_button = ttk.Button(
+    app,
+    text="Get State",
+    command=Bridge.getstate
 
+)
+getstate_button.pack()
+
+
+# START OF TESTING SECTION
+
+# Downlock Simulation
+
+def setdl():
+    Bridge.isdown = dlv.get()
+    if Bridge.isdown:
+        uplock_switch.configure(state="disable")
+    else:
+        uplock_switch.configure(state="normal")
+
+
+dlv = tk.BooleanVar()
+
+downlock_switch = ttk.Checkbutton(
+    app,
+    text="Downlock",
+    variable=dlv,
+    command=setdl,
+
+)
+
+downlock_switch.pack()
+
+
+# Uplock Simulation
+
+def setul():
+    Bridge.isup = ulv.get()
+    if Bridge.isup:
+        downlock_switch.configure(state="disable")
+    else:
+        downlock_switch.configure(state="normal")
+
+
+ulv = tk.BooleanVar()
+
+uplock_switch = ttk.Checkbutton(
+    app,
+    text="Uplock",
+    variable=ulv,
+    command=setul
+
+)
+uplock_switch.pack()
+
+
+# Uplock Simulation
+
+def setfail():
+    Bridge.isfailed = failv.get()
+
+
+failv = tk.BooleanVar()
+
+failed_switch = ttk.Checkbutton(
+    app,
+    text="Fail",
+    variable=failv,
+    command=setfail
+
+)
+failed_switch.pack()
+
+# END OF TESTING SECTION
+
+# App Main Loop
+app.protocol("WM_DELETE_WINDOW", on_closing)
+app.mainloop()
