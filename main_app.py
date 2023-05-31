f4ce82e9abf510ad1441786056fd9b4c88965e54
NICK_LAPTOP\racic <racicot.nicolas2@gmail.com>
2023-05-31 00:48:16 -0400

    Updated initial commit

--- main_app.py
+++ main_app.py
@@ -1,37 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
+import control
+from control import moving
+
 
 # Creating Main App
 
 app = tk.Tk()
 app.title("DBController")
-app.geometry("800x600")
+app.geometry("250x100")
 
 # Buttons
 
 up_button = ttk.Button(
     app,
     text="UP",
+    command=control.command_up
 )
 up_button.pack()
 
-
 stop_button = ttk.Button(
     app,
-    text="STOP"
+    text="STOP",
+    command=control.stop
+
 )
 stop_button.pack()
 
-
 down_button = ttk.Button(
     app,
-    text="DOWN"
+    text="DOWN",
+
 )
 down_button.pack()
 
-
 # App Main Loop
 app.mainloop()
-
-
 
